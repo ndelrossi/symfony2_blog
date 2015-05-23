b3e9e6cfd5aa1eb09a5227036b697b8ef46f2439
Nick DelRossi <nickwtf@gmail.com>
2015-05-23 08:06:29 -0400

    Finish part 2 of tutorial

--- src/Blogger/BlogBundle/Controller/PageController.php
+++ src/Blogger/BlogBundle/Controller/PageController.php
@@ -21,10 +21,17 @@ class PageController extends Controller
 
         $request = $this->getRequest();
         if ($request->getMethod() == 'POST') {
-            $form->bindRequest($request);
+            $form->bind($request);
 
             if ($form->isValid()) {
-                // Perform some action, such as sending an email
+                $message = \Swift_Message::newInstance()
+                    ->setSubject('Contact enquiry from symblog')
+                    ->setFrom('nickadelrossi@gmail.com')
+                    ->setTo($this->container->getParameter('blogger_blog.emails.contact_email'))
+                    ->setBody($this->renderView('BloggerBlogBundle:Page:contactEmail.txt.twig', array('enquiry' => $enquiry)));
+                $this->get('mailer')->send($message);
+
+                $this->get('session')->getFlashBag()->add('blogger-notice', 'Your contact enquiry was successfully sent. Thank you!');
 
                 // Redirect - This is important to prevent users re-posting
                 // the form if they refresh the page
